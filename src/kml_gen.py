6d4a08c9947f26fe477284efd1169735c949a0fc
Guilhem ROLLAND <guilhem99.rolland@gmail.com>
2023-09-05 21:10:52 +0200

    deal with angle at startup

--- src/kml_gen.py
+++ src/kml_gen.py
@@ -1,8 +1,13 @@
 import random
 import math
 
-def generate_kml_random_loop_with_start(start_point, distance_km, num_waypoints):
-    circunference_km = 40075
+
+def generate_kml_random_loop_with_start(start_point, distance_km, num_waypoints, start_cap):
+
+    CIRCUNFERENCE_KM = 40075
+    # Compute the start angle in radians
+    start_angle = (-1) * (start_cap - 90) * 2 * math.pi / 360
+
     # Generate the KML file
     kml_file = open("flight_random_loop_with_start.kml", "w", encoding="utf-8")
     kml_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
@@ -27,36 +32,40 @@ def generate_kml_random_loop_with_start(start_point, distance_km, num_waypoints)
     kml_file.write('\t\t\t<MultiGeometry>\n')
 
     # Invert latitude and longitude to match x and y format
-    prev_point = [0,0]
-    prev_point[0],prev_point[1] = start_point[1],start_point[0]
+    prev_point = [0, 0]
+    prev_point[0], prev_point[1] = start_point[1], start_point[0]
 
     # Build the loop
     for i in range(num_waypoints):
         # Get the angle depending on the number of waypoints
-        angle = i * 2 * math.pi / num_waypoints 
+        angle = start_angle + (i * 2 * math.pi / num_waypoints)
         # Compute the x and y distances
-        x_dist = distance_km * math.cos(angle) / circunference_km * num_waypoints
-        y_dist = distance_km * math.sin(angle) / circunference_km * num_waypoints
+        x_dist = distance_km * \
+            math.cos(angle) / CIRCUNFERENCE_KM * num_waypoints
+        y_dist = distance_km * \
+            math.sin(angle) / CIRCUNFERENCE_KM * num_waypoints
         # Compute the perfect intermediate point
         intermediate_point = (prev_point[0] + x_dist, prev_point[1] + y_dist)
-        
+
         # Get random drift
-        x_drift = random.uniform(-0.001, 0.001)  
-        y_drift = random.uniform(-0.001, 0.001)  
+        x_drift = random.uniform(-0.02, 0.02)
+        y_drift = random.uniform(-0.02, 0.02)
         # Add the drift to the intermediate point
-        intermediate_point = (intermediate_point[0] + x_drift, intermediate_point[1] + y_drift)
+        intermediate_point = (
+            intermediate_point[0] + x_drift, intermediate_point[1] + y_drift)
 
         # Add to the KML file
         kml_file.write('\t\t\t\t<LineString>\n')
         kml_file.write('\t\t\t\t\t<coordinates>\n')
         kml_file.write(f'\t\t\t\t\t\t{prev_point[0]},{prev_point[1]},0\n')
-        kml_file.write(f'\t\t\t\t\t\t{intermediate_point[0]},{intermediate_point[1]},0\n')
+        kml_file.write(
+            f'\t\t\t\t\t\t{intermediate_point[0]},{intermediate_point[1]},0\n')
         kml_file.write('\t\t\t\t\t</coordinates>\n')
         kml_file.write('\t\t\t\t</LineString>\n')
 
         # Update the new starting point for the next iteration
         prev_point = intermediate_point
-    
+
     kml_file.write('\t\t\t</MultiGeometry>\n')
     kml_file.write('\t\t</Placemark>\n')
 
@@ -64,8 +73,12 @@ def generate_kml_random_loop_with_start(start_point, distance_km, num_waypoints)
     kml_file.write('</kml>\n')
     kml_file.close()
 
-# Exemple d'utilisation
-start_point = (45.43894, 5.51026)  # Coordonnées GPS de départ (à adapter)
-distance_km = 50  # kilomètres (boucle complète)
-num_waypoints = 20  # Nombre de points intermédiaires
-generate_kml_random_loop_with_start(start_point, distance_km, num_waypoints)
+
+# Example
+start_point = (45.533329, 5.33333)  # (latitude, longitude) starting point
+distance_km = 50  # Distance of the loop (in km) (it's better to minimize it)
+num_waypoints = 20  # Number of waypoints
+start_cap = 150  # Cap when you pass the starting point (in degrees)
+
+generate_kml_random_loop_with_start(
+    start_point, distance_km, num_waypoints, start_cap)
