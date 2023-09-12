0ef30c311c9fab66152b9e9904d66142e01925e5
Guilhem ROLLAND <guilhem99.rolland@gmail.com>
2023-09-12 19:32:52 +0200

    backup picture name

--- src/photos_pdf_creator.py
+++ src/photos_pdf_creator.py
@@ -4,7 +4,6 @@ from PIL import Image
 from reportlab.pdfgen import canvas
 from reportlab.lib import utils
 
-
 # Chemin du dossier contenant les images
 path = "images/"
 
@@ -17,24 +16,29 @@ selected_files = random.sample(files, 12)
 # Crée un nouveau fichier PDF
 pdf = canvas.Canvas("photos.pdf")
 
-# Ajoute chaque image au PDF
-for i, filename in enumerate(selected_files):
-    # Ouvre l'image et redimensionne-la si nécessaire
-    image = Image.open(path + filename)
-    if image.width > 290 or image.height > 290:
-        image.thumbnail((290, 290))
+# Ouvre un fichier texte pour écrire les noms de fichiers
+with open("photo_list.txt", "w") as file:
+    # Ajoute chaque image au PDF
+    for i, filename in enumerate(selected_files):
+        # Écrit le nom de fichier dans le fichier texte
+        file.write(filename + "\n")
 
-    # Calcule la position de l'image sur la page
-    x = ((i % 6) % 2) * 300 + 5
-    y = ((i % 6) // 2) * 300 + 5
+        # Ouvre l'image et redimensionne-la si nécessaire
+        image = Image.open(path + filename)
+        if image.width > 290 or image.height > 290:
+            image.thumbnail((290, 290))
 
-    # Convertit l'image PIL en objet ReportLab
-    img_data = utils.ImageReader(path + filename)
-    pdf.drawImage(img_data, x, y, width=image.width, height=image.height)
+        # Calcule la position de l'image sur la page
+        x = ((i % 6) % 2) * 300 + 5
+        y = ((i % 6) // 2) * 300 + 5
 
-    # Ajoute une nouvelle page si nécessaire
-    if i == 5:
-        pdf.showPage()
+        # Convertit l'image PIL en objet ReportLab
+        img_data = utils.ImageReader(path + filename)
+        pdf.drawImage(img_data, x, y, width=image.width, height=image.height)
+
+        # Ajoute une nouvelle page si nécessaire
+        if i == 5:
+            pdf.showPage()
 
 # Enregistre le fichier PDF et ferme-le
 pdf.save()
