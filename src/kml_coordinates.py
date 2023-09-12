3b881998f59de3d4834b4ee6a72f492d1f816dc6
Guilhem ROLLAND <guilhem99.rolland@gmail.com>
2023-09-12 20:05:53 +0200

    coordinates has main

--- src/kml_coordinates.py
+++ src/kml_coordinates.py
@@ -1,18 +1,21 @@
 import xml.etree.ElementTree as ET
 
+
 def extract_coordinates(fichier_kml="junction_flight_random_loop.kml"):
     # Charger le fichier KML
     tree = ET.parse(fichier_kml)
     root = tree.getroot()
 
     # Trouver tous les éléments 'LineString'
-    line_strings = root.findall('.//{http://earth.google.com/kml/2.0}LineString')
+    line_strings = root.findall(
+        './/{http://earth.google.com/kml/2.0}LineString')
 
     coordonnees_gps = set()  # Utiliser un ensemble pour stocker les coordonnées uniques
 
     # Parcourir chaque 'LineString' pour extraire les coordonnées
     for line_string in line_strings:
-        coordinates = line_string.find('{http://earth.google.com/kml/2.0}coordinates').text
+        coordinates = line_string.find(
+            '{http://earth.google.com/kml/2.0}coordinates').text
         coordinates = coordinates.strip().split('\n')
 
         # Parcourir chaque paire de coordonnées
@@ -23,9 +26,12 @@ def extract_coordinates(fichier_kml="junction_flight_random_loop.kml"):
 
     return list(coordonnees_gps)
 
-# Exemple d'utilisation de la fonction
-coordonnees = extract_coordinates()
 
-# Afficher les coordonnées extraites sans doublons
-for coordonnee in coordonnees:
-    print(f"Latitude: {coordonnee[0]}, Longitude: {coordonnee[1]}")
+if __name__ == "__main__":
+
+    # Exemple d'utilisation de la fonction
+    coordonnees = extract_coordinates()
+
+    # Afficher les coordonnées extraites sans doublons
+    for coordonnee in coordonnees:
+        print(f"Latitude: {coordonnee[0]}, Longitude: {coordonnee[1]}")
